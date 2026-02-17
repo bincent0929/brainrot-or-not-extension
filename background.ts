b2e8bf101170c7fc7b99567151d2515cba66a534
Vincent Roberson <ghub@varmail.org>
2026-02-16 17:47:50 -0800

    first commit

---
+++ background.ts
@@ -0,0 +1,11 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch") && changeInfo.status === "complete") {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+  }
+});
