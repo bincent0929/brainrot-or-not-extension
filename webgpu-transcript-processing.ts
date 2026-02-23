beeb263f473b1d1370748d86c621acb7a9366e4b
Vincent <git@varmail.org>
2026-02-23 11:00:01 -0800

    separated the model load into another function. got the data saved in json

--- webgpu-transcript-processing.ts
+++ webgpu-transcript-processing.ts
@@ -9,9 +9,13 @@ import { ChatWebLLM } from "@langchain/community/chat_models/webllm";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { InitProgressReport } from '@mlc-ai/web-llm';
 
-import type { video_rating, video_rating_and_info } from './types';
+import transcriptsAndPrompt from "./transcripts.json";
 
-async function processTranscript(): Promise<void> {
+const transcript = transcriptsAndPrompt.transcript2;
+
+const prePrompt = transcriptsAndPrompt.prePrompt;
+
+async function modelLoad() {
     const model = new ChatWebLLM({
         model: "Llama-3.2-1B-Instruct-q4f16_1-MLC",
         chatOptions: {
@@ -24,7 +28,11 @@ async function processTranscript(): Promise<void> {
         console.log(progress);
     });
 
-    const prePrompt = `You will evaluate whether the transcript you are given is worth the time of the user. If it's educational or productive, you'll give it a high score. If it's for entertainment or pleasure, give it a lower score. The goal is to help the user get an idea of whether they're better off watching something else with their time. You'll score it from 0.0 to 5.0. With 0.0 being purely entertainment or "brainrot"; like a live stream recording or Reality TV episode. And 5.0 being something educational, productive, or powerful like a University lecture, TED Talk, or otherwise. Respond ONLY with a JSON object in this format: {"score": <float>, "reason": "<brief reason>"}`
+    return model;
+}
+
+async function processTranscript(): Promise<void> {
+    const model = await modelLoad();
 
     // Call the model with a message and await the response.
     console.log("Running the model on the transcript...")
