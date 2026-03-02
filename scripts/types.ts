78970e51796c1f80de392224b9f60ec067027ed7
Vincent <git@varmail.org>
2026-03-02 13:16:32 -0800

    cleaned up the types

--- scripts/types.ts
+++ scripts/types.ts
@@ -1,23 +1,3 @@
-/**
- * For debugging without a page to grab the title
- * and channel from.
- */
-export interface video_rating {
-  "video_id": string,
-  "transcript": string,
-  "model_used": string,
-  "score": Number,
-}
-
-/**
- * For grabbing all the video info when
- * the page is loading in the extension.
- */
-export interface video_rating_and_info extends video_rating {
-  "video_title": string,
-  "channel_name": string,
-}
-
 export interface videoAnalaysisMessageType {
   type: "GRAB_VIDEO_INFO" | "ANALYZE" | "ANALYSIS_FINISHED";
 }
@@ -29,7 +9,7 @@ export interface youtubeDataAnalysisMessage extends videoAnalaysisMessageType {
   vidId: string;
 }
 
-export type videoEval = {
+export interface videoEval extends videoAnalaysisMessageType {
   "score": number;
   "reason": string;
 }
